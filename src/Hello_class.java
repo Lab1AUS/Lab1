c52ce1b734586cebee6104a78c246205209fa2c6
Khumam <humams123@gmail.com>
2023-01-31 18:14:39 +0400

    Added hello world and feature B

--- src/Hello_class.java
+++ src/Hello_class.java
@@ -3,7 +3,12 @@ public class Hello_class {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("hello world");
 	}
-
+	public static void featureB() {
+		
+		System.out.println("This is B");
+	}
+	
 }
+
